3addadf68ee47333f72e405bcf13813424592363
Ntokozo <ntokozo.majozi@apex-rts.co.za>
2018-08-29 15:30:25 +0200

    updating routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { WorkplaceComponent } from './workplace/workplace.component';
 import { CustomersComponent } from './customers/customers.component';
 import { ArtisansComponent } from './artisans/artisans.component';
 import { VehiclesComponent } from './vehicles/vehicles.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
@@ -15,10 +16,36 @@ import { VehiclesComponent } from './vehicles/vehicles.component';
     WorkplaceComponent,
     CustomersComponent,
     ArtisansComponent,
-    VehiclesComponent
+    VehiclesComponent,
+    NotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: '/Workplace', pathMatch: 'full'
+        },
+      {
+        path: 'Customer', component: CustomersComponent
+      },
+      {
+        path: 'Workplace', component: WorkplaceComponent 
+      },
+      {
+        path: 'Artisan', component: ArtisansComponent
+      },
+      {
+        path: 'Vehicles', component: VehiclesComponent
+      },
+      {
+        path: '404', component: NotfoundComponent
+      },
+      {
+        path: '**', redirectTo: '404'
+      }
+    
+      
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
